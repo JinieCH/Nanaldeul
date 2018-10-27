fc2865a93fc7edc7f610d884a822dbffb3793aa2
sohyeonAn <ansh4393@gmail.com>
2018-10-28 00:32:16 +0900

    create a project

---
+++ app/src/main/java/com/example/nanaldeul/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nanaldeul;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
